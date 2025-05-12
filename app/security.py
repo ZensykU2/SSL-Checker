d98f0c5643e0885193e0abecb235dc7fe7d798b5
ZensykAltDel <cristian.torre@kauz.ch>
2025-05-12 14:20:11 +0200

    Update: Added two new variables for the env file for easier customisation for used hash algorithm and token expire time.

--- app/security.py
+++ app/security.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440))
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
    
