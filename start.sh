b153d1bee0b6b27316ee0c7175a6de9254a7315a
ZensykAltDel <cristian.torre@kauz.ch>
2025-05-14 14:09:52 +0200

    fix: Startup for Tailwind

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "Migrationen werden ausgeführt..."
 alembic -c /app/alembic.ini upgrade head
 
 echo "Baue Tailwind css..."
-npx @tailwindcss/cli -i ./app/static/css/input.css -o ./app/static/css/output.css -o
+npx @tailwindcss/cli -i ./app/static/css/input.css -o ./app/static/css/output.css
 
 echo "Starte FastAPI..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000
